41efc4e8b0846b003de12799188ee7651aec399b
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2024-12-07 22:46:38 +0100

    ports, vcpkg.json: [Enh] Add litefx as a vcpkg dependency

---
+++ ports/litefx/portfile.cmake
@@ -0,0 +1,32 @@
+set(VCPKG_USE_HEAD_VERSION ON)
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO crud89/litefx
+  HEAD_REF main 
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        dx12    LITEFX_BUILD_DIRECTX_12_BACKEND
+        vulkan  LITEFX_BUILD_VULKAN_BACKEND
+        glm     LITEFX_BUILD_WITH_GLM
+        dx-math LITEFX_BUILD_WITH_DIRECTX_MATH
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}/src
+    OPTIONS 
+        -DCMAKE_INSTALL_INCLUDEDIR=include/
+        -DLITEFX_BUILD_EXAMPLES=OFF
+        ${FEATURE_OPTIONS} 
+)
+
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup(CONFIG_PATH cmake/)
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE "${CURRENT_PACKAGES_DIR}/LICENSE" "${CURRENT_PACKAGES_DIR}/NOTICE" "${CURRENT_PACKAGES_DIR}/debug/LICENSE" "${CURRENT_PACKAGES_DIR}/debug/NOTICE")
